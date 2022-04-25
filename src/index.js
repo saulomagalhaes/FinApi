48a021e9fbad87ca17c938a067b0e6ca8a7d23fd
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-04-25 16:47:10 -0300

    feat: req 2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const customers = [];
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
+  
   const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
@@ -15,11 +16,24 @@ app.post('/account', (request, response) => {
   if (customerAlreadyExists) {
     return response.status(400).json({ error: 'Customer already exists' });
   }
+
   const id = uuidv4(); // Gera um id aleatorio(biblioteca uuid)
 
   customers.push({ cpf, name, id, statement: [] });
 
   return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((costumer) => costumer.cpf === cpf);
+
+  if(!customer){
+    return response.status(400).json({error: 'Customer not found'})
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
